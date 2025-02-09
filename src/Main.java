f6734a129d89b3e1531291aebf5455496c929d97
Svitlana Zhuhan <svitlana.zhuhan@gmail.com>
2025-02-09 20:21:45 +0200

    Added code to aqa/lection4

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+
+    public static void printSquare(int number) {
+        System.out.println("Квадрат числа " + number + " = " + (number * number));
+    }
+
+    public static double cylinderVolume(double radius, double height) {
+        return Math.PI * Math.pow(radius, 2) * height;
+    }
+
+    public static int power(int a, int b) {
+        return (int) Math.pow(a, b);
+    }
+
+    public static void main(String[] args) {
+        printSquare(5);
+
+        double volume = cylinderVolume(3.5, 10);
+        System.out.println("Об'єм циліндра: " + volume);
+
+        int result = power(2, 5);
+        System.out.println("2^5 = " + result);
+    }
 }
+
