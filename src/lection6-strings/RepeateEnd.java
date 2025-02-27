63dfe416f64a68ba7c02c3a1372bde0d788727d7
Svitlana Zhuhan <svitlana.zhuhan@gmail.com>
2025-02-27 20:19:26 +0200

    Added changes to files MixString.java and RepeateEnd.java

---
+++ src/lection6-strings/RepeateEnd.java
@@ -0,0 +1,13 @@
+public class RepeateEnd {
+
+    public static String repeatEnd(String str, int n) {
+        String lastNChars = str.substring(str.length() -n);
+        return lastNChars.repeat(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(repeatEnd("Hello", 3));
+        System.out.println(repeatEnd("Hello", 2));
+        System.out.println(repeatEnd("Hello", 1));
+    }
+}
