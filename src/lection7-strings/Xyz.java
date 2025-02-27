90c49f49dc20ebd616f692dabd9291d6323b9899
Svitlana Zhuhan <svitlana.zhuhan@gmail.com>
2025-02-27 21:08:54 +0200

    Added changes to file Xyz.java

---
+++ src/lection7-strings/Xyz.java
@@ -0,0 +1,19 @@
+public class Xyz {
+    public static boolean xyzThere(String str) {
+        // Перебираємо всі можливі входження "xyz"
+        for (int i = 0; i <= str.length() - 3; i++) {
+            if (str.substring(i, i + 3).equals("xyz")) {
+                if (i == 0 || str.charAt(i - 1) != '.') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(xyzThere("abcxyz"));
+        System.out.println(xyzThere("abc.xyz"));
+        System.out.println(xyzThere("xyz.abc"));
+    }
+}
