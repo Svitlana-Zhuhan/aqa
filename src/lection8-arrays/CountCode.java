decdd61bdb7155d02d423fbee4f1d37bcad9be48
Svitlana Zhuhan <svitlana.zhuhan@gmail.com>
2025-03-13 16:19:42 +0200

    Added file CountCode.java

---
+++ src/lection8-arrays/CountCode.java
@@ -0,0 +1,19 @@
+public class CountCode {
+    public static int countCode(String str) {
+        if (str == null || str.length() < 4) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < str.length() - 3; i++) {
+            if (str.substring(i, i + 2).equals("co") && str.charAt(i + 3) == 'e') {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(countCode("aaacodebbb"));
+        System.out.println(countCode("codexxcode"));
+        System.out.println(countCode("cozexxcope"));
+    }
+}
