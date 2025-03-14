f2d322a3e242c019dc104cb49938a2aa0265e1c5
Svitlana Zhuhan <svitlana.zhuhan@gmail.com>
2025-03-14 19:44:19 +0200

    Added file SumWithoutUnlucky13.java

--- src/lection8-arrays/SumWithoutUnlucky13.java
+++ src/lection8-arrays/SumWithoutUnlucky13.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SumWithoutUnlucky13 {
     public static int sumWithoutUnlucky13(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -25,8 +27,12 @@ public class SumWithoutUnlucky13 {
         int[] nums2 = {1, 1};
         int[] nums3 = {1, 2, 2, 1, 13};
 
-        System.out.println("Сума в nums1: " + sumWithoutUnlucky13(nums1));
-        System.out.println("Сума в nums2: " + sumWithoutUnlucky13(nums2));
-        System.out.println("Сума в nums3: " + sumWithoutUnlucky13(nums3));
+        int[] copy1 = Arrays.copyOf(nums1, nums1.length);
+        int[] copy2 = Arrays.copyOf(nums2, nums2.length);
+        int[] copy3 = Arrays.copyOf(nums3, nums3.length);
+
+        System.out.println("Сума в nums1: " + sumWithoutUnlucky13(copy1));
+        System.out.println("Сума в nums2: " + sumWithoutUnlucky13(copy2));
+        System.out.println("Сума в nums3: " + sumWithoutUnlucky13(copy3));
     }
 }
